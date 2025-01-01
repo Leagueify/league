9e11ec55ba9f259f59f4b2ed6dd1fdf83549aef7
Michael duBois <mdubois@bytement.com>
2024-12-31 22:51:13 -0700

    Initialize League Microservice Repository

---
+++ cmd/league.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/leagueify/league/internal/config"
+	"github.com/leagueify/league/internal/integrations/server"
+)
+
+func main() {
+	cfg := config.GetConfig()
+
+	server.NewServer(cfg).Start()
+}
